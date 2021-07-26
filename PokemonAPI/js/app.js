148a6c4ae36d32f6c9bb42c2aa7df318514226ed
Gerardo Vega <54243381+Gerardo703@users.noreply.github.com>
2021-07-26 14:05:30 -0300

    Actualización tamaños
    Se modifican los tamaños de la card para distintos dispositivos

--- PokemonAPI/js/app.js
+++ PokemonAPI/js/app.js
@@ -65,7 +65,7 @@ function mostrarDatos(datos){
     const{id, base_experience, types, name, sprites, stats} = datos;
 
     const datosPokemon = document.createElement('div');
-    datosPokemon.classList.add('card', 'rounded-3', 'border', 'border-5', );
+    datosPokemon.classList.add('card', 'rounded-3', 'border', 'border-3', );
 
     datosPokemon.innerHTML = `
         <div class="bg-dark text-white card-header">
@@ -74,32 +74,32 @@ function mostrarDatos(datos){
         <div class="card-body">
             <div class="row">
                 <div class="col-12 col-sm-12 col-md-12 col-lg-6">
-                    <img  class="img-fluid logo" width="150" height="150" src="${sprites.other.dream_world.front_default}"></img>
+                    <img  class="img-fluid logo mb-2" width="150" height="150" src="${sprites.other.dream_world.front_default}"></img>
                     <h5 class=" card-subtitle rounded mt-2 text-uppercase text-white bg-dark">Type: ${types[0].type.name}</h5>  
                 </div>
                 <div class="col-12 col-sm-12 col-md-12 col-lg-6">
                     <h5 class=" card-subtitle rounded mt-1 text-uppercase text-white bg-dark">STATS</h5>
                     <div class="row my-3 justify-content-center">
                         <div class="col-5">
-                            <h6 class=" card-subtitle mt-2 text-center text-uppercase text-dark">${stats[0].stat.name}: <span class="text-danger">${stats[0].base_stat}</span></h6>
+                            <h4 class=" card-subtitle mt-2 text-center text-uppercase text-dark">${stats[0].stat.name}: <span class="text-danger">${stats[0].base_stat}</span></h4>
                         </div>
                         <div class="col-5">
-                            <h6 class=" card-subtitle mt-2 text-center text-uppercase text-dark">ATK: <span class="text-danger">${stats[2].base_stat}</span></h6>
+                            <h4 class=" card-subtitle mt-2 text-center text-uppercase text-dark">ATK: <span class="text-danger">${stats[2].base_stat}</span></h4>
                         </div>
                         <div class="col-5">
-                            <h6 class=" card-subtitle mt-2  text-center text-uppercase text-dark">SATK: <span class="text-danger">${stats[3].base_stat}</span></h6>
+                            <h4 class=" card-subtitle mt-2  text-center text-uppercase text-dark">SATK: <span class="text-danger">${stats[3].base_stat}</span></h4>
                         </div>
                         <div class="col-5">
-                            <h6 class=" card-subtitle mt-2 text-center text-uppercase text-dark">XP:  <span class="text-primary">${base_experience}</span></h6>
+                            <h4 class=" card-subtitle mt-2 text-center text-uppercase text-dark">XP:  <span class="text-primary">${base_experience}</span></h4>
                         </div>
                         <div class="col-5">
-                            <h6 class=" card-subtitle mt-2 text-center text-uppercase text-dark">DFS: <span class="text-primary">${stats[2].base_stat}</span></h6>
+                            <h4 class=" card-subtitle mt-2 text-center text-uppercase text-dark">DFS: <span class="text-primary">${stats[2].base_stat}</span></h4>
                         </div>
                         <div class="col-5">
-                            <h6 class=" card-subtitle mt-2 text-center text-uppercase  text-dark">SDFS: <span class="text-primary">${stats[4].base_stat}</span></h6>
+                            <h4 class=" card-subtitle mt-2 text-center text-uppercase  text-dark">SDFS: <span class="text-primary">${stats[4].base_stat}</span></h4>
                         </div>
                         <div class="col-5">
-                            <h6 class=" card-subtitle mt-2 text-center text-uppercase text-dark">SPD: ${stats[5].base_stat}</h6>
+                            <h4 class=" card-subtitle mt-2 text-center text-uppercase text-dark">SPD: ${stats[5].base_stat}</h6>
                         </div>
                     </div>
                 </div>
